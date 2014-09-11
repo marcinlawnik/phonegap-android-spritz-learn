defc16727a94c425e3c6af2f66bc0b95960ca87d
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-09-11 12:35:12 +0200

    Add some things from tutorial

--- js/views/HomeView.js
+++ js/views/HomeView.js
@@ -13,9 +13,15 @@ var HomeView = function(store) {
 
 
     this.findByName = function() {
-        console.log('findByName');
         store.findByName($('.search-key').val(), function(employees) {
             $('.employee-list').html(HomeView.liTemplate(employees));
+            if (self.iscroll) {
+                console.log('Refresh iScroll');
+                self.iscroll.refresh();
+            } else {
+                console.log('New iScroll');
+                self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: true });
+            }
         });
     };
 
