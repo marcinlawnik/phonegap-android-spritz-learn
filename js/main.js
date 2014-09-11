bdb6644e366bec91f4640bbbef3ca9a2901f0ade
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-09-11 11:52:36 +0200

    Add some things from tutorial

--- js/main.js
+++ js/main.js
@@ -8,30 +8,14 @@ var app = {
         }
     },
 
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
 
-
-    findByName: function() {
-        console.log('findByName');
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
     initialize: function() {
         var self = this;
-        //Compile templates
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
         this.store = new MemoryStore(function() {
             self.showAlert('Store Initialized', 'Info');
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
